7aadbd3c8731fdf7c3c1e5b50d1889c71cc50507
douglas-sola <v671408@gmail.com>
2024-08-24 16:50:17 -0300

    adicionando e aprendendo operadores sritimeticos

--- aula/14.py
+++ aula/14.py
@@ -7,7 +7,7 @@ e = n1 ** n2
 di = n1 // n2
 res = n1 % n2
 
-print ("a soma é {},\n e o produto é {},\n e a divisão é {:.4}\n".format(s , m , d))
+print ("a soma é {},\n e o produto é {},\n e a divisão é {:.4}\n".format(s , m , d), end="")
 
 print ("a potenciação vale {:.5},\n e a divisão inteira vale {}\n".format(e , di))
 

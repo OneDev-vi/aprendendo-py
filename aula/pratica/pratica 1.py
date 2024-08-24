0b654b7fd21fa9d4db86140a08080b32e7253a52
douglas-sola <v671408@gmail.com>
2024-08-24 15:10:48 -0300

    cracha pc

---
+++ aula/pratica/pratica 1.py
@@ -0,0 +1,7 @@
+import math
+#dolar para real
+#valor1 = int(input("dijite um valor: "))
+#dolar = 5.49 / valor1
+
+#print ("o valor que voçê tem para dolar vai ficar {}".format(dolar))
+print (2 + (3))

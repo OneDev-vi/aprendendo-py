09d12c585d27f97c2064f17cd52684f167a5c8c0
douglas-sola <v671408@gmail.com>
2024-08-24 18:31:17 -0300

    aaaa quelo manga

---
+++ aula/desafio de python/desafio 4.py
@@ -0,0 +1,6 @@
+cresente = input("um valor: ")
+antecesor = input("outro valor: ")
+a = antecesor - 1
+print ("a o cresente é {} e o antesesor é {} manga {}".format(cresente , antecesor , a))
+
+#nã sei fazer algu simples kkkkkk

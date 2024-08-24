63684e21427f97c35946bcc190ee354398e68495
douglas-sola <v671408@gmail.com>
2024-08-24 08:58:43 -0300

    adicionando uma nova função para o projeto de python

---
+++ aula/10.py
@@ -0,0 +1,4 @@
+valor1 = int(input("dijite um valor: "))
+valor2 = int(input("dijite outro valor: "))
+somar  = valor1 + valor2
+print ("o primeiro dijito que voçê informou foi {} e o outro numero foi {} e a soma é {}".format(valor1 , valor2 , somar))
--- aula/desafio de python/desafio 3.py
+++ aula/desafio de python/desafio 3.py
@@ -1,5 +1,5 @@
-a = int(input("nada: "))
-b = int(input("nada: "))
+a = int(input("dijite um valor: "))
+b = int(input("dijite outro valor: "))
 s = a + b
 
 print("A soma dos dois números é: {}".format(s))

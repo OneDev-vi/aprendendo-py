d04ede1daeaeb03bc2ecf28e1b80bbd7ab59645f
douglas-sola <v671408@gmail.com>
2024-08-25 08:14:42 -0300

    desadio .py

--- aula/desafio de python/desafio 12.py
+++ aula/desafio de python/desafio 12.py
@@ -2,4 +2,4 @@ larg = float(input("qual a largura da parede?: "))
 alto = float(input("qual a altura da parade?: "))
 area = larg * alto
 z = area / 2
-print (f"sua parade tem uma dimenção de {area} e a quantidade de tnta que voçê precisara sera {z}")
+print (f"sua parade tem uma dimenção de {larg * alto} e a quantidade de tinta que voçê precisara sera {area / 2}")
--- aula/desafio de python/desafio 14.py
+++ aula/desafio de python/desafio 14.py
@@ -1,4 +1,4 @@
 salario = float(input("qual o salario da funcionario? R$"))
 novo = salario * 15 /100
 soma = salario + novo
-print (f"o salario éra de {salario} e com o aumento vai ficar com {novo} e somano {soma}")
+print (f"o salario éra de {salario} e com o aumento vai ficar com {novo * 15 /100} e somano {salario + soma}")

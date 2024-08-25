9a4a22830e8983e28edcea0c11535373058e723e
douglas-sola <v671408@gmail.com>
2024-08-25 03:46:05 -0300

    sal é um codigo copiado de uma pypi que é a jax ok
    mais o outro foi feito porm

---
+++ aula/desafio de python/desafio 12.py
@@ -0,0 +1,5 @@
+larg = float(input("qual a largura da parede?: "))
+alto = float(input("qual a altura da parade?: "))
+area = larg * alto
+z = area / 2
+print (f"sua parade tem uma dimenção de {area} e a quantidade de tnta que voçê precisara sera {z}")
---
+++ aula/desafio de python/desafio 9.py
@@ -0,0 +1,5 @@
+dis = float(input("dijite uma distancia em km: "))
+me = dis * 100
+cm = dis * 1000
+mm = dis * 10000
+print (f"em metros ficara {dis*100} e em cm ficara {dis * 1000} e em melimetros ficara {dis * 10000}")

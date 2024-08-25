fe54c27e770c248d7b77df0bcafe9bd3e95f93d8
douglas-sola <v671408@gmail.com>
2024-08-24 22:52:01 -0300

    adicionando arquivo de desafio 6 e aprimarando minhas abilidades em python vama nessA!!!!

---
+++ aula/desafio de python/desafio 5.py
@@ -0,0 +1,4 @@
+numero = int(input("Dijite um valor: "))
+#a = numero - 1
+#s = numero + 1
+print ("o antecesor do numero que voçê dijitou é {}\n e o numero que voçê dijito é {}\n e o sucesor é {}".format((numero-1),numero,(numero+1) ))

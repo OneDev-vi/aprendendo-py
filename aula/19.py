09351b6b4adb36c35fb4ec933f50d12eeed3f786
douglas-sola <v671408@gmail.com>
2024-08-25 08:36:25 -0300

    agua

--- aula/19.py
+++ aula/19.py
@@ -1,3 +1,6 @@
 import math
-num = int(input("Dijite um numero: "))
-raiz = math.sqrt
+num = float(input("Dijite um numero: "))
+raiz = math.sqrt(num)
+ceil = math.ceil(num)
+flopo = math.floor(num)
+print (f"a raiz  de {num} vai ser de {math.sqrt(num)} \n e a do ciel que eu não sei oque que é kkkk {math.ceil(num)} \n e do factorial via ser \n {math.floor(num)}")

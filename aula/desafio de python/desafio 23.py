e3e6b8d8d19caa06961c1ce67dd03ecc7b33a3a9
douglas-sola <v671408@gmail.com>
2024-08-25 16:28:02 -0300

    vini

---
+++ aula/desafio de python/desafio 23.py
@@ -0,0 +1,2 @@
+seunome = str(input("Qual é seu nome?> "))
+print (f"seu nome é maiuscolo é {seunome.upper()}\n é agora seu nome em minuscolo é {seunome.lower()}\n e seu nome tem um total de {len(seunome)}")

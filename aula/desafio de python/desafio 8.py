77c25f775b8cb1c2c20d4f0a1b83723823a3216e
douglas-sola <v671408@gmail.com>
2024-08-24 23:47:58 -0300

    sou foda!!!

---
+++ aula/desafio de python/desafio 8.py
@@ -0,0 +1,4 @@
+nota1 = float(input("qual a nota do aluno?: "))
+nota2 = float(input("qual a nota do segundo aluno?: "))
+print (f"a nota do primeiro aluno é {nota1} e a do segundo aluno é {nota2} e samndo da {nota1-nota2}")
+

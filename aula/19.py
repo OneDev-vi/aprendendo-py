98333066a04a5119917fc1fa1b049963c9f9c5e7
douglas-sola <v671408@gmail.com>
2024-08-25 08:37:17 -0300

    sal

--- aula/19.py
+++ aula/19.py
@@ -4,3 +4,4 @@ raiz = math.sqrt(num)
 ceil = math.ceil(num)
 flopo = math.floor(num)
 print (f"a raiz  de {num} vai ser de {math.sqrt(num)} \n e a do ciel que eu não sei oque que é kkkk {math.ceil(num)} \n e do factorial via ser \n {math.floor(num)}")
+#manga

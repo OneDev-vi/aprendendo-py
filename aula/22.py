380ed93c46d733791754dba9e186036fd42762c8
douglas-sola <v671408@gmail.com>
2024-08-25 15:52:54 -0300

    manga

--- aula/22.py
+++ aula/22.py
@@ -1,12 +1,30 @@
 frase = "Mangericão Com Salada"
-print (len(frase))
+dividir = frase.split()
+dividindo = frase.split()
+print (dividir)
+print (frase[12])
+print (frase[1:3])
+print (frase[:3])
+print (frase[::3])
+print (frase[2:5:3])
+print (frase[3:])
+print (frase[3::])
+print (frase.find("Com"))
+print (frase.lower())
+print (frase.upper())
+print (frase[9::3])
+print (frase.replace("Com", "e agua com gas e"))
+#print (frase.count)
+print (dividindo[2][3])
+#frase = frase.replace("Com" , "com gas e")
+#print (frase.replace("Com", "manga").lower().find("manga"))
 
 
 
 
-print ('''sb dkvasvdfs,vhdfs v,ds md\n vm,d vsd bd bsd vfsdbvmdsvg 
-efvefsvefvevbegbrsebrwbrswbrsbrstwbwrbwrtbgrwtbrwsbtrwbrtrwbtrbwrdf
-gkeglthbrwhkgvbhretbkgrkhvtrejvbgrvhbgrkehgbvbtrkvhrwvbghjrtbvhjrtbvjrvgf
-rrlhrhgrtlehrlehgrkhbgrelgrkejlgbkjerlgkrtljgrelhgkrtluklyegfinallydfb gf
-fvhwkvjtlgvvbj,hv,vbhjfrhjmvremrjhmbvtrjhvbwrjvbhmfvbhumnfrvbmfrvhjrmvbjtrmvbrmvfgbbdf
-wvbrwhvulrwhvlrwvhliurvhrwkigvkvruvhjrtvjlksvhrlkjbvhrbjkgrbkjrlubrigburkiubgkiurkiurnhkiugrbb dfg''')
+#'''print ('''sb dkvasvdfs,vhdfs v,ds md\n vm,d vsd bd bsd vfsdbvmdsvg 
+#efvefsvefvevbegbrsebrwbrswbrsbrstwbwrbwrtbgrwtbrwsbtrwbrtrwbtrbwrdf
+#gkeglthbrwhkgvbhretbkgrkhvtrejvbgrvhbgrkehgbvbtrkvhrwvbghjrtbvhjrtbvjrvgf
+#rrlhrhgrtlehrlehgrkhbgrelgrkejlgbkjerlgkrtljgrelhgkrtluklyegfinallydfb gf
+#fvhwkvjtlgvvbj,hv,vbhjfrhjmvremrjhmbvtrjhvbwrjvbhmfvbhumnfrvbmfrvhjrmvbjtrmvbrmvfgbbdf
+#wvbrwhvulrwhvlrwvhliurvhrwkigvkvruvhjrtvjlksvhrlkjbvhrbjkgrbkjrlubrigburkiubgkiurkiurnhkiugrbb dfg''')'''

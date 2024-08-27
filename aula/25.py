3da60f72529c7f19493e72d76a2415368e653f66
douglas-sola <v671408@gmail.com>
2024-08-27 16:36:37 -0300

    suco

---
+++ aula/25.py
@@ -0,0 +1,15 @@
+ano = input("voçê esta aqui?> ")
+
+if (ano == "sim"):
+    print ('A intão voçê esta aqui')
+    
+elif ano == " ":
+    print ("........")
+    
+elif (ano == "não" , "nao"):
+    print ("A ENTÃO VOÇÊ ESTA AQUI!!!!!!!!!")
+    
+elif (ano == "s" , "n"):
+    print (".........cara não se escreve assim")
+elif (ano == "manga"):
+    print ("vai toma suco cacete")
---
+++ aula/desafio de python/desafio 28.py
@@ -0,0 +1,3 @@
+n = str(input("qual é seu nome?: ")).strip()
+nome = n.split()
+print (f"prazer por te conhecer!!!!!!!!!!!!!!!!!\n seu primeiro nome é {nome[0]}\n e seu ultimo nome é {nome[len(nome)-1]}")

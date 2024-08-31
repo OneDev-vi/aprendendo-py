5d4668bcd19dc3623f09e459276365dda71309d8
douglas-sola <v671408@gmail.com>
2024-08-31 13:31:49 -0300

    manga

--- aula/26.py
+++ aula/26.py
@@ -1,13 +1,7 @@
-n1 = float(input("qual a nota do primeiro aluno?> "))
-n2 = float(input("qual a nota do segundo aluno?> "))
-so = (n1 + n2)/2
-
-if (so >= 7.0):
-    print ("sua media foi boa!!")
-    print ("parabens!!")
-elif (so <= 5.0):
-    print ("sua media foi ruim!!")
-    print ("tente novamente!!")
-elif (so >= 6.5):
-    print ("sua media foi mediana!!")
-    print ("continue assim!!")
+frase = "mandioca com salada e arroz e de sobremesa mandioca"
+numero = 5
+numero2 = 6
+if (numero > numero2):
+    len(frase)
+else :
+    len(frase)

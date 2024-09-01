0ab6672a51845b56edababf49620045c4b099d7e
douglas-sola <v671408@gmail.com>
2024-09-01 17:43:56 -0300

    soal

---
+++ aula/desafio de python/desafio 43.py
@@ -0,0 +1,16 @@
+print ("-=-"*20)
+print ("TRIANGULO")
+print ("-=-" * 20)
+r1 = float(input("primeiro segimento> "))
+r2 = float(input("segundo segmento> "))
+r3 = float(input("terceiro segmento> "))
+if (r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r1):
+    print ("Os segmentos acima PODEM FORMAR triangulo")
+    if r1 == r2 == r3:
+        print ("Triangulo equilatero")
+        if r1 != r2 != r3 != r1:
+            print ("escaleno")
+        else :
+            print ("isosceles")
+else:
+    print ("Os segmentos acima NÃo PODEM FORMAR triagulo")
